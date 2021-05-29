21962e71d6dfa9c1043ce7a0e3724252dfe44415
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-05-29 14:10:02 -0300

    Update mp.js

--- app/controllers/mp.js
+++ app/controllers/mp.js
@@ -10,7 +10,7 @@ module.exports.inicio = function(application, req, res){
             axios
             .post('https://api.mercadopago.com/checkout/preferences?access_token=TEST-7360931303703983-082420-403bd4058ccb931c56962684437df3e7-44599713', 
                 {   
-                "notification_url":"https://webhook.site/2035bc56-1123-4e6b-8871-64ebfe713853",
+                "notification_url":"http://52.67.220.198:3000/recebepost",
                 "external_reference": get['external_reference'],
             "items":    [
                             {
@@ -56,30 +56,28 @@ module.exports.inicio = function(application, req, res){
                 console.log('post enviará');
             axios
             .post('http://localhost:3000/recebepost', 
-            {   
-            "notification_url":"https://webhook.site/2035bc56-1123-4e6b-8871-64ebfe713853",
-            "external_reference": '',
-        "items":    [
-                        {
-                        "title": 'title',
-                        "description": 'title',
-                        "quantity":1,
-                        "currency_id":"BRL",
-                        "unit_price":'',
-                        "picture_url":"https://poderdalinguagem.com/inscricao/img/logo%20curso.png"
-                        }
-                    ]
-            }
+            {
+                "action": "payment.created1",
+                "api_version": "v1",
+                "data": {
+                  "id": "1237151624"
+                },
+                "date_created": "2021-05-27T22:08:24Z",
+                "id": 7455388554,
+                "live_mode": false,
+                "type": "payment",
+                "user_id": "44599713"
+              }
             
             )
             .then((res) => {
                 //console.log(`statusCode: ${res.statusCode}`)
                 
                 
-                console.log(res.data);
+                //console.log(res.data);
                 //link=res.data.sandbox_init_point;
                 //res.render(res.data['sandbox_init_point']);
-                console.log('********************************************************************************')
+                //console.log('********************************************************************************')
                 console.log('post sent')
                 link=res.data;
                 //res.render(res.data['sandbox_init_point']);
@@ -88,7 +86,7 @@ module.exports.inicio = function(application, req, res){
 
             })
             .catch((error) => {
-                console.error(error)
+                //console.error(error)
                 console.log('********************************************************************************')
                 console.log('error')
             })
@@ -104,10 +102,19 @@ module.exports.inicio = function(application, req, res){
             //////////////////////////////////////////////////////////////
             module.exports.recebepost = function(application, req, res){
 
-                
-                var get=req.query;
-                
-                var connection = application.config.dbConnection();   
+                var data = '';
+                req.on('data', function(chunk) { 
+                  data += chunk;
+                });
+                req.on('end', function() {
+                  req.rawBody = data;
+                  console.log (req.rawBody);
+
+                  const obj = JSON.parse(req.rawBody);
+                  console.log (obj.data.id);
+
+
+                  var connection = application.config.dbConnection();   
             
                 var mpModel = new application.app.models.mpDAO(connection);       
                 
@@ -115,9 +122,23 @@ module.exports.inicio = function(application, req, res){
                         //console.log(result);
                         //res.redirect('/pedidos');;
                         };        
-                mpModel.retorno(1, callback);
+                mpModel.retorno(obj.data.id, callback);
 
-                res.render('home/ajax1.ejs');         
+                res.render('home/ajax1.ejs');
+                  
+                });
+
+
+                
+                //var get=res.query;
+                //var post = req.body;
+                //var get = req.query
+
+                //console.log (post);
+                ;
+                
+                
+                         
 
 
 
