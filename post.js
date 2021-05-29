c7a0564218fae20647626b54d935146afcc77315
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-05-29 14:10:17 -0300

    Create post.js

---
+++ post.js
@@ -0,0 +1,11 @@
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Access the parse results as request.body
+app.post('/', function(request, response){
+    console.log(request.body.user.name);
+    console.log(request.body.user.email);
+});
