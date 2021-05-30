04116c8894bc394259e6da7667f7714979ad79d0
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-05-30 13:58:28 -0300

    Update mpDAO.js

--- app/models/mpDAO.js
+++ app/models/mpDAO.js
@@ -8,9 +8,15 @@ mpDAO.prototype.retorno = function (pedidoid, callback1){
     this._connection.query('INSERT INTO mp (ref) VALUES ('+pedidoid+')');;
 }
 
-mpDAO.prototype.adicionar = function (pagamento, callback){ 
-    this._connection.query('INSERT INTO arquivos SET ?', pagamento, callback);
+mpDAO.prototype.links = function (mes, callback){
+    this._connection.query('SELECT * FROM cobrancas WHERE mes = '+mes, callback);  
 }
+
+mpDAO.prototype.link = function (id, callback){
+    
+    this._connection.query('SELECT * FROM cobrancas WHERE id = '+id, callback);  
+}
+
 
 mpDAO.prototype.updateUpload = function (id, z,callback){ 
     this._connection.query('UPDATE arquivos SET img=1, tipo="'+z+'" WHERE id= '+ id, callback);
